1690de5bd5df0312d5bd31b23a041f8eb8a67589
vitalixek <vitalixek@yandex.ru>
2017-05-16 22:42:38 +0300

    Work in progress

--- Classifier/classifier.cpp
+++ Classifier/classifier.cpp
@@ -1,2 +1,145 @@
-#include<iostream>
+#pragma once
+
+#include <iostream>
+#include <string>
+#include <vector>
+
 #include "classifier.h"
+
+using namespace std;
+
+//Int
+ScaleInt::ScaleInt()
+{
+	computational_ = true;
+}
+bool ScaleInt::isComputational()
+{
+	return computational_;
+}
+void ScaleInt::setValue(int value)
+{
+	value_ = value;
+}
+int ScaleInt::getValue()
+{
+	return value_;
+}
+//Double
+ScaleDouble::ScaleDouble()
+{
+	computational_ = true;
+}
+bool ScaleDouble::isComputational()
+{
+	return computational_;
+}
+void ScaleDouble::setValue(double value)
+{
+	value_ = value;
+}
+double ScaleDouble::getValue()
+{
+	return value_;
+}
+//Text
+ScaleText::ScaleText()
+{
+	computational_ = false;
+}
+bool ScaleText::isComputational()
+{
+	return computational_;
+}
+void ScaleText::setValue(string value)
+{
+	value_ = value;
+}
+string ScaleText::getValue()
+{
+	return value_;
+}
+//DictionaryObject
+DictionaryObject::DictionaryObject(string name, string type, string scale)
+{
+	name_ = name;
+	type_ = type;
+	if (scale == "NumericInt")
+		scale_ = new ScaleInt();
+	else if (scale == "NumericDouble")
+		scale_ = new ScaleDouble();
+	else if (scale == "Text")
+		scale_ = new ScaleText();
+}
+DictionaryObject::~DictionaryObject()
+{
+	name_.clear();
+	type_.clear();
+	delete scale_;
+}
+string DictionaryObject::getName()
+{
+	return name_;
+}
+string DictionaryObject::getType()
+{
+	return type_;
+}
+Scale* DictionaryObject::getScale()
+{
+	return scale_;
+}
+
+Dictionary::Dictionary()
+{
+
+}
+Dictionary::~Dictionary()
+{
+
+}
+void Dictionary::insert(DictionaryObject object)
+{
+	objects_.push_back(object);
+	if (object.getScale()->isComputational())
+		computational_++;
+	if (object.getType() == "Attribute")
+		attributes_count_++;
+	else if (object.getType() == "Class")
+		classes_count_++;
+	count_++;
+}
+int Dictionary::find(string name)
+{
+	for (int i = 0; i < objects_.size(); i++)
+		if (objects_[i].getName() == name)
+			return i;
+	return -1;
+}
+void Dictionary::remove(string name)
+{
+	if (objects_[find(name)].getScale()->isComputational())
+		computational_--;
+	if (objects_[find(name)].getType() == "Attribute")
+		attributes_count_--;
+	else if (objects_[find(name)].getType() == "Class")
+		classes_count_--;
+	count_--;
+	objects_.erase(objects_.begin() + find(name));
+}
+int Dictionary::getCount()
+{
+	return count_;
+}
+int Dictionary::getAttributesCount()
+{
+	return attributes_count_;
+}
+int Dictionary::getClassesCount()
+{
+	return classes_count_;
+}
+int Dictionary::getComputationalCount()
+{
+	return computational_;
+}
--- Classifier/classifier.h
+++ Classifier/classifier.h
@@ -1,14 +1,19 @@
 #pragma once
 
-#include<iostream>
-#include<string.h>
+#include <iostream>
+#include <string>
+#include <vector>
+
+#include "D:\project\demods-master\DSCore\DSCore\DSCore.h"
 
 using namespace std;
 
 class Classifier
 {
 public:
-	virtual void method();
+	virtual ~Classifier(){}
+
+	virtual void method() = 0;
 
 };
 
@@ -23,3 +28,96 @@ class ClearClassifier : public Classifier
 private:
 
 };
+
+class Scale
+{
+public:
+	Scale() {};
+	~Scale() {};
+	virtual bool isComputational() = 0;
+protected:
+	bool computational_;
+};
+
+class ScaleInt : public Scale
+{
+public:
+	ScaleInt();
+	~ScaleInt() {};
+	bool isComputational();
+	void setValue(int value);
+	int getValue();
+private:
+	int value_;
+};
+
+class ScaleDouble : public Scale
+{
+public:
+	ScaleDouble();
+	~ScaleDouble() {};
+	bool isComputational();
+	void setValue(double value);
+	double getValue();
+private:
+	double value_;
+};
+
+class ScaleText : public Scale
+{
+public:
+	ScaleText();
+	~ScaleText() {};
+	bool isComputational();
+	void setValue(string value);
+	string getValue();
+private:
+	string value_;
+};
+
+class DictionaryObject
+{
+public:
+	DictionaryObject(string name, string type, string scale);
+	~DictionaryObject();
+	string getName();
+	string getType();
+	Scale* getScale();
+private:
+	string name_, type_;
+	Scale* scale_;
+};
+
+class Dictionary
+{
+public:
+	Dictionary();
+	~Dictionary();
+	void insert(DictionaryObject object);
+	int find(string name);
+	void remove(string name);
+	int getCount();
+	int getAttributesCount();
+	int getClassesCount();
+	int getComputationalCount();
+
+private:
+	int count_ = 0;
+	int attributes_count_=0;
+	int classes_count_ = 0;
+	int computational_ = 0;
+	vector<DictionaryObject> objects_;
+};
+
+class Teacher
+{
+public:
+	Teacher() {};
+	~Teacher() {};
+	
+private:
+	Dictionary dictionary_;
+};
+
+
+
